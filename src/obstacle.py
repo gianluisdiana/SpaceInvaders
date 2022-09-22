e6b00bde0f1f64c734279d7feb2fa2f6f914bca6
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2022-09-22 15:16:01 +0100

    feat(obstacle): `Obstacle` class to represent a group of blocks

--- src/obstacle.py
+++ src/obstacle.py
@@ -20,3 +20,26 @@ class Block(pygame.sprite.Sprite):
         self.image = pygame.Surface((side_length, side_length))
         self.image.fill(color)
         self.rect = self.image.get_rect(topleft=pos)
+
+
+class Obstacle(pygame.sprite.Group):
+    """Represents an obstacle.
+
+    Attributes:
+        block_size (int): The length of the block's side.
+        color (tuple[int, int, int]): The RGB color of the block.
+        pos (tuple[int, int]): The position of the block (x, y).
+    """
+
+    def __init__(self, block_size: int, color: tuple[int], pos: tuple[int]):
+        """Initialize the obstacle with the blocks to display and its position.
+
+        Args:
+            side_length (int): The length of the block's side.
+            color (tuple[int, int, int]): The RGB color of the block.
+            pos (tuple[int, int]): The position of the block (x, y).
+        """
+        super().__init__()
+        self.block_size = block_size
+        self.color = color
+        self.pos = pos

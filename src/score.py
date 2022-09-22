28df5021714ffbd015295570def5ce6549478afb
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2022-09-22 19:38:28 +0100

    feat(score): `Score` class for the game score and font added

---
+++ src/score.py
@@ -0,0 +1,36 @@
+from pygame import Surface
+from pygame.font import Font
+
+class Score:
+    """Score class for the game.
+
+    Attributes:
+        pos (tuple[int, int]): The x and y position of the score.
+        color (tuple[int, int, int]): The RGB color code.
+        value (int): The current score.
+        font (pygame.font.Font): The font of the message displayed.
+    """
+
+    def __init__(self, pos: tuple[int], color: tuple[int]) -> None:
+        """Initialize the score class.
+
+        Args:
+            pos (tuple[int, int]): The x and y position of the score.
+            color (tuple[int, int, int]): The RGB color code.
+        """
+        self.pos = pos
+        self.color = color
+        self.value = 0
+        self.font = Font('../fonts/Pixeled.ttf', 20)
+
+    def increase(self, value: int = 1) -> None:
+        """Increase the score.
+
+        Args:
+            value: An integer indicating the value to increase the score.
+        """
+        self.value += value
+
+    def reset(self) -> None:
+        """Reset the score."""
+        self.value = 0

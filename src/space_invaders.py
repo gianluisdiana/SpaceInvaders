bb566329ae501428a55a40caf9d0acced879c3b5
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2022-09-22 15:28:20 +0100

    feat(game): drawing the obstacles

--- src/space_invaders.py
+++ src/space_invaders.py
@@ -64,6 +64,7 @@ class SpaceInvaders:
         """Draw all the images in the screen."""
         self.screen.fill((30, 30, 30))
         self.player.draw(self.screen)
+        self.obstacles.draw(self.screen)
         self.player.sprite.lasers.draw(self.screen)
 
     def run(self) -> None:

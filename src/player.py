c31557291889769e3fec0f0dc67c025e7591bcee
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2022-09-22 13:37:37 +0100

    feat(player): get_input method to handle player's input (move player)

--- src/player.py
+++ src/player.py
@@ -23,3 +23,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('../images/player.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom = pos)
         self.speed = speed
+
+    def get_input(self) -> None:
+        """Get the player's input and move the player."""
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        elif keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed

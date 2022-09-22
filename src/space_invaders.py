53c5d5b0b71dc11d1be8de142dfaa72e712b7aeb
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2022-09-22 13:18:04 +0100

    feat(init): clock and fps attributes

--- src/space_invaders.py
+++ src/space_invaders.py
@@ -6,16 +6,23 @@ class SpaceInvaders:
     Attributes:
         screen (pygame.Surface):
             The screen on which the game will be displayed.
+        clock (pygame.time.Clock):
+            The clock that will be used to control the game's FPS.
+        fps (int):
+            How many FPS the game will run at.
     """
 
-    def __init__(self, size: tuple[int]):
+    def __init__(self, size: tuple[int], fps: int = 60):
         """Initialize all the requirements for the game.
 
         Args:
             size (tuple[int, int]): The size of the screen (width, height).
+            fps (int, optional): The FPS of the game. Defaults to 60.
         """
         pygame.init()
         self.screen = pygame.display.set_mode(size)
+        self.clock = pygame.time.Clock()
+        self.fps = fps
 
     def run(self) -> None:
         """Start the game loop."""

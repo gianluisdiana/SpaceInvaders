352b7380ada0625161b9ad995f8dec9594474139
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2022-09-27 23:57:51 +0100

    feat(lives): attribute to set the initial amount of lives

--- src/lives.py
+++ src/lives.py
@@ -4,9 +4,14 @@ class Lives:
     """Represents the remaining lives of the player.
 
     Attributes:
-        amount (int): The number of lives the player has.
-        image (pygame.Surface): The image that will be displayed.
-        pos (tuple[int]): The position of the images.
+        amount (int):
+            The position of the images.
+        inital_amount (int):
+            The initial amount of lives the player has.
+            The number of lives the player has.
+        image (pygame.Surface):
+            The image that will be displayed.
+        pos (tuple[int]):
     """
 
     def __init__(self, amount: int, screen_width: int):
@@ -16,7 +21,7 @@ class Lives:
             amount (int): The number of lives the player has.
             screen_width (int): The width of the game screen.
         """
-        self.amount = amount
+        self.initial_amount = self.amount = amount
         self.image = pygame.image.load('./images/player.png').convert_alpha()
         x = screen_width - self.image.get_width() - 10
         self.pos = (x, 10)
@@ -38,3 +43,7 @@ class Lives:
         for live in range(self.amount - 1):
             x = self.pos[0] - live * (self.image.get_width() + 10)
             screen.blit(self.image, (x, self.pos[1]))
+
+    def reset(self) -> None:
+        """Reset the number of lives to the initial value."""
+        self.amount = self.initial_amount

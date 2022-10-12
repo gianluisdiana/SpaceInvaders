a1cb670aa49bd9edb2d267686a1ee0df67a59a10
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2022-10-12 11:44:16 +0100

    feat(main): function to check if it wants to use a custom configuration

--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
+from os import path
 from space_invaders import SpaceInvaders
+
+def check_custom(argv: tuple[str]) -> str:
+    """Checks if the user wants to use a custom configuration.
+
+    Args:
+        argv (tuple[str]): The command line arguments.
+
+    Returns:
+        str: The custom configuration folder's name formated.
+    """
+    if not path.exists('./custom'):
+        raise FileNotFoundError('The folder for the custom configurations does not exist.')
+
+    if (len(argv) != 3 or argv[1] != "--custom"):
+        return './'
+
+    if not path.exists(f'./custom/{argv[2]}'):
+        raise FileNotFoundError(f'The custom folder "{argv[2]}" does not exist.')
+
+    return f'./custom/{argv[2]}/'
 
 def main():
     game = SpaceInvaders((600, 600))
